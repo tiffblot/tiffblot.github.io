a59825cbd9380a42ca25595f50fb9a8a6048c1f2
jynnie <jynnie@mit.edu>
2020-06-13 20:19:16 -0700

    First build

---
+++ precache-manifest.8d6197a3573ecd25e1cb7bd932c242c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284078d0895cf5cd36c14eb372791132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0c960c6c1a618b5de8",
+    "url": "/static/css/main.e4aec8fc.chunk.css"
+  },
+  {
+    "revision": "ea5649bb514c44db805c",
+    "url": "/static/js/2.704a0d4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.704a0d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0c960c6c1a618b5de8",
+    "url": "/static/js/main.935aaa56.chunk.js"
+  },
+  {
+    "revision": "d15370a228ca2680aaec",
+    "url": "/static/js/runtime-main.cd5d966d.js"
+  }
+]);
