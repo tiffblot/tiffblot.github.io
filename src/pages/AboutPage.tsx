9f7f1b7bfc9c46752345dc51e4f6c32062454ffc
Jessica 'jynnie' Tang <jynn.tang@gmail.com>
2025-11-09 21:04:39 -0800

    Update About

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -31,54 +31,55 @@ export const AboutPage = () => {
       </p>
 
       <h3>Press</h3>
-      <p>
-        <a
-          href="https://corporate.comcast.com/stories/comcast-nbcuniversal-celebrates-brilliance-courage-aapi-communities"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Comcast Connections through Creations
-        </a>
-        , AAPIHM 2023
-        <br />
-        <a
-          href="https://usa.kinokuniya.com/blog-top/kinokuniya-atlanta-grand-opening"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kinokuniya Atlanta Grand Opening Artist Alley
-        </a>{" "}
-      </p>
+      <div className="flexColumn gap-1 child-noMargin">
+        <p>
+          <a
+            href="https://corporate.comcast.com/stories/comcast-nbcuniversal-celebrates-brilliance-courage-aapi-communities"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Comcast Connections through Creations
+          </a>
+          , AAPIHM 2023
+        </p>
+        <p>
+          <a
+            href="https://usa.kinokuniya.com/blog-top/kinokuniya-atlanta-grand-opening"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kinokuniya Atlanta Grand Opening Artist Alley
+          </a>
+        </p>
+      </div>
 
       <h3>Conventions</h3>
-      <p>
-        Anime NYC 2024 (New York City, NY)
-        <br />
-        Dreamhack ATL 2024 (Atlanta, GA)
-        <br />
-        JapanFest 2023 (Duluth, GA) with Musubee ATL
-        <br />
-        MomoCon 2023 (Atlanta, GA)
-        <br />
-        Anime Weekend Atlanta 2022 (Atlanta, GA)
-      </p>
+      <div className="flexColumn gap-1 child-noMargin">
+        <p>Anime Weekend Atlanta 2025 (Atlanta, GA)</p>
+        <p>Dreamhack ATL 2025 (Atlanta, GA)</p>
+        <p>JapanFest 2025 (Duluth, GA) with Rebel Teahouse</p>
+        <p>Anime NYC 2024 (New York City, NY)</p>
+        <p>Dreamhack ATL 2024 (Atlanta, GA)</p>
+        <p>JapanFest 2023 (Duluth, GA) with Musubee ATL</p>
+        <p>MomoCon 2023 (Atlanta, GA)</p>
+        <p>Anime Weekend Atlanta 2022 (Atlanta, GA)</p>
+      </div>
 
       <h3>Exhibits</h3>
-      <p>
-        Hi I&apos;m Home 2024 Chunky Things (Los Angeles, CA)
-        <br />
-        Being Ourselves 2023 Asian Cultural Empowerment (Atlanta, GA)
-        <br />
-        Labor of Love: Dumpling Fest 2023 Asian American Advocacy Fund (Atlanta,
-        GA)
-        <br />
-        Have A Nice Day 2023 Chunky Things (Los Angeles, CA)
-        <br />
-        Year of the Rabbit 2023 The Bakery (Atlanta, GA)
-        <br />
-        Hua Yuan: AAPI Artist in Bloom 2023 Roswell Cultural Arts Center
-        (Atlanta, GA)
-      </p>
+      <div className="flexColumn gap-1 child-noMargin">
+        <p>Hi I&apos;m Home 2024 Chunky Things (Los Angeles, CA)</p>
+        <p>Being Ourselves 2023 Asian Cultural Empowerment (Atlanta, GA)</p>
+        <p>
+          Labor of Love: Dumpling Fest 2023 Asian American Advocacy Fund
+          (Atlanta, GA)
+        </p>
+        <p>Have A Nice Day 2023 Chunky Things (Los Angeles, CA)</p>
+        <p>Year of the Rabbit 2023 The Bakery (Atlanta, GA)</p>
+        <p>
+          Hua Yuan: AAPI Artist in Bloom 2023 Roswell Cultural Arts Center
+          (Atlanta, GA)
+        </p>
+      </div>
     </Box>
   );
 };
